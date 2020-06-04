13f036f551e0fe0914dd3b85ac7d1d6858e799af
“Stacy <stacyrays@gmail.com>
2020-06-04 14:58:16 -0700

    fixed the sign up page

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -30,9 +30,12 @@ export default class UserSignUp extends Component {
             <h2 className="validation--errors--label">Validation errors</h2>
             <div className="validation-errors">
               <ul>
-                <li className="passwordMatch">
-                  Passwords do not match. Please try again.
+                <li className="firstNameDOM">Must provide a first name.</li>
+                <li className="lastNameDOM">Must provide a last name.</li>
+                <li className="emailAddressDOM">
+                  Must provide an email address.
                 </li>
+                <li className="passwordDOM">Must provide a password.</li>
               </ul>
             </div>
           </div>
@@ -115,13 +118,58 @@ export default class UserSignUp extends Component {
       confirmPassword,
     } = this.state;
 
+    //Create variables for HTML elements that display errors
+    const errorHeader = document.getElementsByClassName(
+      "validation--errors--label"
+    )[0];
+    const errorsContainer = document.getElementsByClassName(
+      "validation-errors"
+    )[0];
+    const errorList = errorsContainer.getElementsByTagName("UL")[0];
+    const firstNameDOM = document.getElementsByClassName("firstNameDOM")[0];
+    const lastNameDOM = document.getElementsByClassName("lastNameDOM")[0];
+    const emailAddressDOM = document.getElementsByClassName(
+      "emailAddressDOM"
+    )[0];
+
+    const passwordDOM = document.getElementsByClassName("passwordDOM")[0];
+
+    errorHeader.setAttribute("style", "display:none");
+    errorList.setAttribute("style", "display:none");
+    firstNameDOM.setAttribute("style", "display:none");
+    lastNameDOM.setAttribute("style", "display:none");
+    emailAddressDOM.setAttribute("style", "display:none");
+    passwordDOM.setAttribute("style", "display:none");
+
+    if (firstName === "") {
+      console.log("provide a first name");
+      errorList.setAttribute("style", "display:block");
+      firstNameDOM.setAttribute("style", "display:block");
+    }
+    if (lastName === "") {
+      console.log("provide a last name");
+      errorList.setAttribute("style", "display:block");
+      lastNameDOM.setAttribute("style", "display:block");
+    }
+    if (emailAddress === "") {
+      console.log("provide email address");
+      errorList.setAttribute("style", "display:block");
+      emailAddressDOM.setAttribute("style", "display:block");
+    }
+    if (password === "") {
+      console.log("provide a password");
+      errorList.setAttribute("style", "display:block");
+      passwordDOM.setAttribute("style", "display:block");
+    }
+
     if (
       firstName === "" ||
       lastName === "" ||
       emailAddress === "" ||
       password === ""
     ) {
-      this.props.history.push("/error");
+      //this.props.history.push("/error");
+      return null;
     } else {
       if (password === confirmPassword) {
         // Create user
