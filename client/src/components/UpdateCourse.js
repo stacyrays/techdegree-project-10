dd7a4f21f8d4546cc2ea5d2e9c45bd1bf6c73932
“Stacy <stacyrays@gmail.com>
2020-06-03 18:50:25 -0700

    updatecourse now goes to forbidden if not owner

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
 import Form from "./Form";
 
-export default class CreateCourse extends Component {
+export default class UpdateCourse extends Component {
   state = {
     course: {},
     owner: {},
@@ -10,22 +11,33 @@ export default class CreateCourse extends Component {
   async componentDidMount() {
     try {
       const { context } = this.props;
+      const authUser = context.authenticatedUser;
       const course = await context.data.getCourse(this.props.match.params.id);
       const owner = course.owner;
-
       console.log("This is the course " + owner.firstName);
-
-      if (course) {
+      if (owner.id === authUser.id && course) {
         this.setState({ course, owner });
       } else {
         console.log("there is an error 404");
-        this.props.history.push("/notfound");
+        this.props.history.push("/forbidden");
       }
     } catch (err) {
       console.log("there is an error 404");
       this.props.history.push("/error");
     }
   }
+
+  //   if (course) {
+  //     this.setState({ course, owner });
+  //   } else {
+  //     console.log("there is an error 404");
+  //     this.props.history.push("/notfound");
+  //   }
+  // } catch (err) {
+  //   console.log("there is an error 404");
+  //   this.props.history.push("/error");
+  // }
+
   render() {
     const { course, owner, errors } = this.state;
     const { firstName, lastName, id } = owner;
@@ -36,92 +48,95 @@ export default class CreateCourse extends Component {
     const authUser = context.authenticatedUser;
 
     console.log(authUser.id + " " + id);
-
-    return (
-      <div className="bounds course--detail">
-        <h1>Update Course</h1>
-        <div>
+    if (authUser.id === id) {
+      return (
+        <div className="bounds course--detail">
+          <h1>Update Course</h1>
           <div>
-            <h2 className="validation--errors--label">Validation errors</h2>
-            <div className="validation-errors">
-              <ul>
-                <li className="provide-title"></li>
-                <li className="provide-desc"></li>
-              </ul>
+            <div>
+              <h2 className="validation--errors--label">Validation errors</h2>
+              <div className="validation-errors">
+                <ul>
+                  <li className="provide-title"></li>
+                  <li className="provide-desc"></li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <Form
-            cancel={this.cancel}
-            errors={errors}
-            submit={this.submit}
-            submitButtonText="Update Course"
-            elements={() => (
-              <React.Fragment>
-                <div className="grid-66">
-                  <div className="course--header">
-                    <h4 className="course--label">Course</h4>
-                    <input
-                      id="title"
-                      name="title"
-                      type="text"
-                      ref={(input) => (this.title = input)}
-                      defaultValue={course.title}
-                      className="input-title course--title--input"
-                    />
-                    <p>
-                      Author: {firstName} {lastName}
-                    </p>
-                  </div>
-                  <div className="course--description">
-                    <div>
-                      <textarea
-                        id="description"
-                        name="description"
+            <Form
+              cancel={this.cancel}
+              errors={errors}
+              submit={this.submit}
+              submitButtonText="Update Course"
+              elements={() => (
+                <React.Fragment>
+                  <div className="grid-66">
+                    <div className="course--header">
+                      <h4 className="course--label">Course</h4>
+                      <input
+                        id="title"
+                        name="title"
                         type="text"
-                        ref={(input) => (this.description = input)}
-                        defaultValue={course.description}
-                      ></textarea>
+                        ref={(input) => (this.title = input)}
+                        defaultValue={course.title}
+                        className="input-title course--title--input"
+                      />
+                      <p>
+                        Author: {firstName} {lastName}
+                      </p>
+                    </div>
+                    <div className="course--description">
+                      <div>
+                        <textarea
+                          id="description"
+                          name="description"
+                          type="text"
+                          ref={(input) => (this.description = input)}
+                          defaultValue={course.description}
+                        ></textarea>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="grid-25 grid-right">
-                  <div className="course--stats">
-                    <ul className="course--stats--list">
-                      <li className="course--stats--list--item">
-                        <h4>Estimated Time</h4>
-                        <div>
-                          <input
-                            id="estimatedTime"
-                            name="estimatedTime"
-                            type="text"
-                            ref={(input) => (this.estimatedTime = input)}
-                            defaultValue={course.estimatedTime}
-                            className="course--time--input"
-                          />
-                        </div>
-                      </li>
-                      <li className="course--stats--list--item">
-                        <h4>Materials Needed</h4>{" "}
-                        <div>
-                          <textarea
-                            id="materialsNeeded"
-                            name="materialsNeeded"
-                            type="text"
-                            ref={(input) => (this.materialsNeeded = input)}
-                            defaultValue={course.materialsNeeded}
-                            className="course--materials--input"
-                          ></textarea>
-                        </div>
-                      </li>
-                    </ul>
+                  <div className="grid-25 grid-right">
+                    <div className="course--stats">
+                      <ul className="course--stats--list">
+                        <li className="course--stats--list--item">
+                          <h4>Estimated Time</h4>
+                          <div>
+                            <input
+                              id="estimatedTime"
+                              name="estimatedTime"
+                              type="text"
+                              ref={(input) => (this.estimatedTime = input)}
+                              defaultValue={course.estimatedTime}
+                              className="course--time--input"
+                            />
+                          </div>
+                        </li>
+                        <li className="course--stats--list--item">
+                          <h4>Materials Needed</h4>{" "}
+                          <div>
+                            <textarea
+                              id="materialsNeeded"
+                              name="materialsNeeded"
+                              type="text"
+                              ref={(input) => (this.materialsNeeded = input)}
+                              defaultValue={course.materialsNeeded}
+                              className="course--materials--input"
+                            ></textarea>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                </div>
-              </React.Fragment>
-            )}
-          />
+                </React.Fragment>
+              )}
+            />
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return null;
+    }
   }
 
   submit = () => {
